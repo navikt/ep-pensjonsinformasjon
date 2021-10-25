9a4d65a9c6a29f1185c12cc3e4ca89961d77dc57
mariampervez <mariam.pervez@nav.no>
2021-10-25 15:26:33 +0200

    Første utkast til ep-pensjonsinformasjon
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

---
+++ src/main/kotlin/no/nav/eessi/pensjon/pensjonsinformasjon/models/Pensjonstype.kt
@@ -0,0 +1,6 @@
+package no.nav.eessi.pensjon.services.pensjonsinformasjon
+
+data class Pensjontype(
+        val sakId: String,
+        val sakType: String
+)
---
+++ src/test/kotlin/ValidatePensjoninformasjonsTest.kt
@@ -0,0 +1,79 @@
+import no.nav.eessi.pensjon.pensjonsinformasjon.PensjonRequestBuilder
+import no.nav.eessi.pensjon.pensjonsinformasjon.PensjonsinformasjonClient
+import no.nav.eessi.pensjon.pensjonsinformasjon.simpleFormat
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.util.ResourceUtils
+import org.springframework.web.client.RestTemplate
+import io.mockk.every
+import io.mockk.mockk
+
+class ValidatePensjoninformasjonsTest {
+
+
+    private var mockrestTemplate: RestTemplate = mockk()
+
+    private lateinit var pensjonsinformasjonClient: PensjonsinformasjonClient
+
+    @BeforeEach
+    fun setup() {
+        pensjonsinformasjonClient = PensjonsinformasjonClient(mockrestTemplate, PensjonRequestBuilder())
+        pensjonsinformasjonClient.initMetrics()
+    }
+
+
+    @Test
+    fun `Sjekker om pensjoninformasjon XmlCalendar kan være satt eller null sette simpleFormat`() {
+        val mockResponseEntity = createResponseEntityFromJsonFile("classpath:full-generated-response.xml")
+
+        every { mockrestTemplate.exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java)) } returns mockResponseEntity
+        val data = pensjonsinformasjonClient.hentAltPaaVedtak("1243")
+
+        var result = data.ytelsePerMaanedListe.ytelsePerMaanedListe.first()
+
+       assertEquals("2008-02-06", result.fom.simpleFormat())
+       assertEquals("2015-08-04", result.tom?.simpleFormat())
+
+        result = data.ytelsePerMaanedListe.ytelsePerMaanedListe.getOrNull(1)
+
+        Assertions.assertNotNull(result)
+        Assertions.assertNotNull(result.fom)
+
+        assertEquals("2008-02-06", result.fom.simpleFormat())
+        assertEquals(null, result.tom?.simpleFormat())
+
+    }
+
+    @Test
+    fun `Sjekker om pensjoninformasjon plukk ut sak fra en liste over brukersaker`() {
+        val mockResponseEntity = createResponseEntityFromJsonFile("classpath:P2100-GJENLEV-REVURDERING-M-KRAVID-INNV.xml")
+
+        every { mockrestTemplate.exchange(any<String>(), any(), any<HttpEntity<Unit>>(), eq(String::class.java)) } returns mockResponseEntity
+        val data = pensjonsinformasjonClient.hentAltPaaAktoerId("1243")
+
+        val sak = PensjonsinformasjonClient.finnSak("22915550", data)
+
+        assertEquals("22915550", sak?.sakId.toString())
+        assertEquals("UFOREP", sak?.sakType)
+        assertEquals(3, sak?.kravHistorikkListe?.kravHistorikkListe?.size)
+        assertEquals(1, sak?.ytelsePerMaanedListe?.ytelsePerMaanedListe?.size)
+
+    }
+
+    private fun mockAnyRequest(kravLokasjon : String) {
+        val mockResponseEntity = createResponseEntityFromJsonFile(kravLokasjon)
+        every { mockrestTemplate.exchange(any<String>(), any(), any(), eq(String::class.java)) } returns mockResponseEntity
+    }
+
+    private fun createResponseEntityFromJsonFile(filePath: String, httpStatus: HttpStatus = HttpStatus.OK): ResponseEntity<String?> {
+        val mockResponseString = ResourceUtils.getFile(filePath).readText()
+        return ResponseEntity(mockResponseString, httpStatus)
+    }
+
+
+}
