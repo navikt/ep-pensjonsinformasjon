821a346db41b32cb7f1d061c2233ba0f263ed0fa
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2021-10-26 10:30:03 +0200

    min første kommit for ep-personinformasjon

--- src/test/kotlin/ValidatePensjoninformasjonsTest.kt
+++ src/test/kotlin/ValidatePensjoninformasjonsTest.kt
@@ -70,6 +70,7 @@ class ValidatePensjoninformasjonsTest {
         every { mockrestTemplate.exchange(any<String>(), any(), any(), eq(String::class.java)) } returns mockResponseEntity
     }
 
+
     private fun createResponseEntityFromJsonFile(filePath: String, httpStatus: HttpStatus = HttpStatus.OK): ResponseEntity<String?> {
         val mockResponseString = ResourceUtils.getFile(filePath).readText()
         return ResponseEntity(mockResponseString, httpStatus)
